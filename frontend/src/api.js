87160e063dcb619aa279e834be9a57bb1c54d4e3
Tanyajha-2003 <129519581+Tanyajha-2003@users.noreply.github.com>
2025-11-26 16:56:39 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+import axios from 'axios';
+const API = axios.create({ baseURL: "https://course-view-1fd1.onrender.com/api" || '/api' });
+API.interceptors.request.use(config=>{
+  const token = localStorage.getItem('token');
+  if(token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
+export default API;
+
 API.interceptors.request.use(config=>{
   const token = localStorage.getItem('token');
   if(token) config.headers.Authorization = `Bearer ${token}`;
