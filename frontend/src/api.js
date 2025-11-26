d131338373530183186d955ce74ff2ac9fef4d47
Tanyajha-2003 <129519581+Tanyajha-2003@users.noreply.github.com>
2025-11-26 17:01:08 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,9 +7,3 @@ API.interceptors.request.use(config=>{
 });
 export default API;
 
-API.interceptors.request.use(config=>{
-  const token = localStorage.getItem('token');
-  if(token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-export default API;
