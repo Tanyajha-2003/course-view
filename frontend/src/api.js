398c8a244fdf188edbe9800a424e699ad8723ece
Tanyajha-2003 <tanyakumar2003@gmail.com>
2025-11-26 15:46:18 +0530

    Latest commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: process.env.REACT_APP_API_URL
 });
-
 API.interceptors.request.use(config=>{
   const token = localStorage.getItem('token');
   if(token) config.headers.Authorization = `Bearer ${token}`;
